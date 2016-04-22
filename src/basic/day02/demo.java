f4fd03cb6434ca7333e9734fcda5f16d9801f424
array <gjc@hrsoft.net>
2016-04-22 21:53:02 +0800

    从控制台接收输入

--- src/basic/day02/demo.java
+++ src/basic/day02/demo.java
@@ -1,6 +1,7 @@
 package basic.day02;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by array on 16-4-22.
@@ -11,5 +12,12 @@ public class demo {
         int r = random.nextInt(26);  //产生随机数[0,26]
         char c = (char)('A'+r);
         System.out.println(c);
+
+        Scanner scanner = new Scanner(System.in);
+        //从控制台接收一个数字
+        double n = scanner.nextDouble();
+        //处理
+        n = Math.pow(n,2); //将从控制台得到的数平方
+        System.out.println(n);
     }
 }
