36da89f2d6d6f6b3aead94dcdf6db6b849cc4c8d
array <gjc@hrsoft.net>
2016-04-29 20:35:28 +0800

    第四天练习 实现数组复制

--- src/basic/day04/StringDemo.java
+++ src/basic/day04/StringDemo.java
@@ -1,5 +1,7 @@
 package basic.day04;
 
+import java.util.Arrays;
+
 /**
  * Created by array on 16-4-28.
  */
@@ -9,5 +11,11 @@ public class StringDemo {
         char ch = s.charAt(2);
         int a = ch - '0'; //char转int
         System.out.println(a); //7(int)
+
     }
+//    public static void main(String[] args){
+//        int[] arr = {1,2,3,4};
+//        int[] dest = Arrays.copyOf(arr,arr.length);
+//        System.out.println(Arrays.toString(dest));
+//    }
 }
