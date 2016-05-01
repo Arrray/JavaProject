f6748cc31de48345949dcf8383914e86f728f568
高军超 <gjc@hrsoft.net>
2016-05-01 19:31:44 +0800

    第五天练习 插入式排序第一种写法

---
+++ src/basic/day05/SortDemo4.java
@@ -0,0 +1,36 @@
+package basic.day05;
+
+import java.util.Arrays;
+
+/**
+ * Created by Gaojunchao on 16/5/1.
+ */
+public class SortDemo4 {
+    public static void main(String[] args) {
+        int[] ary = {8, 2, 3, 7, 1};
+        ary = insertSort(ary);
+        System.out.println(Arrays.toString(ary));
+    }
+
+    /**
+     * 插入式排序
+     */
+    private static int[] insertSort(int[] ary) {
+        int i, j, t;
+        for (i = 1; i < ary.length; i++) {
+            t = ary[i];
+            //利用循环查找插入位置:到头j=-1
+            for (j=i-1; j>=0; j--)
+                if (t < ary[j]) {
+                    ary[j+1] = ary[j]; //[j]->[j+1]向后移动
+                } else { // t不小于ary[j]
+                    break; //找到插入位置:t不小于ary[j]
+                }
+            ary[j+1] = t; //插入t->[j+1]
+        }
+        return ary;
+    }
+
+}
+
+
